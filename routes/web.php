3a54511502dd0a6154214a831d19ae167469a4e7
sugihiro1 <sugiyama@k00.itscom.net>
2018-08-13 04:30:38 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('sign
 
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');

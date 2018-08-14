2f8cfdd806e52613a39f741633de87f6824056f7
sugihiro1 <sugiyama@k00.itscom.net>
2018-08-14 00:11:33 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('sign
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});

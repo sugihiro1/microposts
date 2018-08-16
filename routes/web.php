f34e01a5d3885fd85c458085ee11b6fdd1b08901
sugihiro1 <sugiyama@k00.itscom.net>
2018-08-16 23:19:41 +0000

    Microspostscontroller amended

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
+
 Route::get('/', 'MicropostsController@index');
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');

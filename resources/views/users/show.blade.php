272bb06bc93c7a4c26565a734bdaf69d15b14dbc
sugihiro1 <sugiyama@k00.itscom.net>
2018-08-16 03:14:25 +0000

    follow, unfollow

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,6 +10,7 @@
                 <div class="panel-body">
                     <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
                 </div>
+                @include('user_follow.follow_button', ['user' => $user])
             </div>
         </aside>
         <div class="col-xs-8">

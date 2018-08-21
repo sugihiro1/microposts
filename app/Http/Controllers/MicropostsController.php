2cd7f19e662a360963121a8224706957b1019b5e
sugihiro1 <sugiyama@k00.itscom.net>
2018-08-21 08:53:55 +0000

    codes for debugging deleted

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -15,7 +15,6 @@ class MicropostsController extends Controller
     {
         $data = [];
         if (\Auth::check()) {
-//dd('MicropostsController index');
             $user = \Auth::user();
             $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <?php $show_favor_button = false; ?>
-    @include('users.users', ['users' => $users, 'show_favor_button'=> $show_favor_button])
+    @include('users.users', ['users' => $users)
 @endsection
